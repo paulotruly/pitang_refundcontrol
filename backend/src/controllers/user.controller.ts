7c4c8ff5578fdb60241bcc687d55763e39b49571
Paulo <paulomsmsms@gmail.com>
2026-04-30 23:16:00 -0300

    criando controller de usuarios

---
+++ backend/src/controllers/user.controller.ts
@@ -0,0 +1,58 @@
+import type { Request, Response } from "express";
+import { hash } from "bcrypt";
+import { prisma } from "../prisma.js"
+
+export const createUser = async (req: Request, res: Response) => {
+    const { nome, email, senha, perfil } = req.body;
+    const hashedPassword = await hash(senha, 10);
+    const user = await prisma.user.create({
+        data: { nome, email, senha: hashedPassword, perfil },
+        omit: { senha: true },
+    });
+    res.status(201).json(user);
+};
+
+export const getUsers = async (_req: Request, res: Response) => {
+    const users = await prisma.user.findMany({ omit: { senha: true } });
+    res.json(users);
+};
+
+export const getUserById = async (req: Request, res: Response) => {
+    const user = await prisma.user.findUnique({
+        where: { id: req.params.id as string },
+        omit: { senha: true },
+    });
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+};
+
+export const updateUser = async (req: Request, res: Response) => {
+    const { nome, email, senha } = req.body;
+    const data: { nome?: string; email?: string; senha?: string } = { nome, email };
+    if (senha) {
+        data.senha = await hash(senha, 10);
+    }
+    try {
+        const updatedUser = await prisma.user.update({
+            where: { id: req.params.id as string },
+            data,
+            omit: { senha: true },
+        });
+        res.json(updatedUser);
+    } catch {
+        return res.status(404).json({ message: "User not found" });
+    }
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+    try {
+        await prisma.user.delete({
+            where: { id: req.params.id as string },
+        });
+        res.status(204).send();
+    } catch {
+        return res.status(404).json({ message: "User not found" });
+    }
+};
